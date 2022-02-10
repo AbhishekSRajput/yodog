8a6a1edc892550f17ec2c3a692a7a1cd2a484471
Abhishek singh <49828900+AbhishekSRajput@users.noreply.github.com>
2022-02-10 15:09:57 +0530

    adding-routes

---
+++ models/yodoger.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+//review model
+const Review = require("./review");
+
+const YoDogersSchema = new Schema({
+  title: String,
+  description: String,
+  location: String,
+  price: Number,
+  image: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+});
+
+YoDogersSchema.post("findOneAndDelete", async (doc) => {
+  if (doc) {
+    await Review.remove({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
+});
+
+module.exports = mongoose.model("Yodoger", YoDogersSchema);
---
+++ validation/YodogerSchema.js
@@ -0,0 +1,11 @@
+const Joi = require("joi");
+
+module.exports.YodogerSchema = Joi.object({
+  yodoger: Joi.object({
+    title: Joi.string().required(),
+    price: Joi.number().required().min(0),
+    image: Joi.string().required(),
+    location: Joi.string().required(),
+    description: Joi.string().required(),
+  }).required(),
+});
